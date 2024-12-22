7e2723dd7026cdd4b7662cc80c40a670513e0b61
missgoin <mister.goindo@gmail.com>
2024-12-22 09:03:40 +0700

    /proc/pid/maps: dont allow paths with lineage to be listed

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -525,6 +525,13 @@ bypass_orig_flow:
 #endif
 	
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+
+struct dentry *dentry = file->f_path.dentry;
+        if (dentry) {
+        	const char *path = (const char *)dentry->d_name.name; 
+            	if (strstr(path, "lineage")) { return; }
+        }
+	
 	}
 
 	start = vma->vm_start;
